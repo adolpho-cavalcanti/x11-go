ae6f4a1135807128d006007122bbc27ac919553c
Adolpho Cavalcanti <adolpho.nascimento@compasso.com.br>
2021-03-13 15:45:40 -0300

    Config Eslint and Prettier

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return 
+    <div>
+      Oiiiiblihyglyahvjsdvjnwejvnewjnvjwencjwn wnvjevjewn wenviwenvikew iovnikewvew
+    </div>;
+  
+}
